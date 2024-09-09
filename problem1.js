7d706d5e5b79f1955b3d0dfecef6059e51133d0a
sahabulislamsifat <sahabulislamsifat@gmail.com>
2024-09-10 01:09:22 +0600

    nothing

---
+++ problem1.js
@@ -0,0 +1,43 @@
+// Amar Kache 1 ta "Array" te kichu number ache , Amake ber korte hobe sob cheye boro number konta
+
+const numbers = [2, 4, 5, 6, 77, 100, 800, 900, 45, 55, 66, 82];
+// console.log(Math.max(...numbers));
+// Let biggest numbers
+
+// bigest = 2;
+// bigest = 4;
+// bigest = 5;
+// bigest = 6;
+// bigest = 45;
+// bigest = 55;
+// bigest = 66;
+// bigest = 77;
+// bigest = 82;
+// bigest = 100;
+// bigest = 800;
+// bigest = 900;
+
+// let biggest = numbers[0];
+// console.log("before loop", biggest);
+
+// for (let i = 1; i < numbers.length; i++) {
+//   if (numbers[i] > biggest) {
+//     biggest = numbers[i];
+//   }
+// }
+// console.log("after loop", biggest);
+
+// let biggest = numbers[0];
+// console.log("before loop", biggest);
+// for (let i = 1; i < numbers.length; i++) {
+//   console.log("current number:", numbers[i], "biggest numbers:", biggest);
+//   if (numbers[i] > biggest) {
+//     biggest = numbers[i];
+//   }
+//   console.log("current biggest:", biggest);
+// }
+// console.log("after loop", biggest);
+
+
+
+function give
