dfbb04b5339265c93d38f26ee8aa6886b3998d56
sahabulislamsifat <sahabulislamsifat@gmail.com>
2024-09-10 00:36:29 +0600

    second

---
+++ function.js
@@ -0,0 +1,71 @@
+// const x = 34;
+// console.log(x);
+
+/*
+
+function function_name() {
+
+
+}
+
+*/
+
+// function showName() {
+//   console.log("Sifat");
+// }
+
+// showName();
+// showName();
+// showName();
+// showName();
+
+// let x = 5;
+// console.log(x);
+// showName();
+
+// function showName(name, age, address, nationality) {
+//   console.log(name,age,address,nationality);
+// }
+
+// showName("Sifat", 22, "Dhaka", "Bangladeshi");
+
+// function add(x, y) {
+//   const total = x + y;
+//   console.log(total);
+// }
+
+// add(5, 6);
+// add(5, 7);
+// add(5, 10);
+
+// function giveName() {
+//   const x = "Sifat";
+//   return x;
+// }
+
+// const myName = giveName();
+// console.log(myName);
+
+// TYPE OF FUNCTIONS [4]
+
+// 1. No Parameters no return
+// function show() {
+//   console.log("hello");
+// }
+
+// 2.  Parameter with no return
+// function add(x, y) {
+//   console.log(x + y);
+// }
+
+// 3. No Parameter but return
+// function giveName() {
+//   const x = "Sifat";
+//   return x;
+// }
+
+// 4. Parameter with return
+
+// function gitDouble(x) {
+//   return x * 2;
+// }
